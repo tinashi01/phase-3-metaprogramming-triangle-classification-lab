38b3eb94ad8d0364a229ed201ce978f59aa12bc7
Tina Shi <init.defaultBranch>
2023-05-15 21:56:44 +1000

    commit tree 271aceea2d9776e379c53651c98877b9ab21ccc2

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,11 @@
 class Triangle
   # write code here
+  def kind(a,b,c)
+    if a==b && b==c 
+      :equilateral
+    end
+  end
+  class TriangleError < StandardError
+    # triangle error code
+  end
 end
